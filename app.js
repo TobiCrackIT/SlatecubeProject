4075e7666c859c9cfa7247f55ae6af097ab1deab
geraldanosike <geraldanosike.javhse@gmail.com>
2018-11-27 16:04:33 +0100

    mlab config

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const express = require("express"),
 
 
       // connect MONGODB
- mongoose.connect("mongodb://localhost/friendsMEET", {useNewUrlParser:true})
+//  mongoose.connect("mongodb://localhost/friendsMEET", {useNewUrlParser:true})
 
       let url = "mongodb://meet:meet1990@ds033679.mlab.com:33679/friendsmeet" ;
 
